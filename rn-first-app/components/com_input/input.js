e59ba64d5fde84959e17164bb1ef94aab1103920
githelsui <githelsuico@gmail.com>
2020-03-28 02:21:41 -0700

    textinputs

--- rn-first-app/components/com_input/input.js
+++ rn-first-app/components/com_input/input.js
@@ -3,14 +3,25 @@ import React, { useState, Component } from "react";
 import { StyleSheet, Text, TextInput, View, Button } from "react-native";
 
 export default function App() {
-  const [outputText, setOutputText] = useState("Open App.js");
+  const [outputText, setOutputText] = useState("Old Message");
+  const [value, onChangeText] = React.useState('Useless Placeholder');
+
   return (
     <View style={styles.container}>
-      <Text>{outputText}</Text>
-      <Button
-        title="Change Text"
+      <Text>Item: </Text>
+      <TextInput
+      style={{ height: 40, borderColor: 'gray', borderWidth: 2 }}
+      onChangeText={text => onChangeText(text)}
+      value={value}
+    />
+
+    <Button
+        title="Add"
         onPress={() => setOutputText("the text changed!")}
       />
+
+      <Text>{outputText}</Text>
+      
     </View>
   );
 }
