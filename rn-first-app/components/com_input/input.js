e7eb72b36e66d61acdc4b1fdd54cca01115d9455
githelsui <githelsuico@gmail.com>
2020-03-28 16:00:08 -0700

    Dd

--- rn-first-app/components/com_input/input.js
+++ rn-first-app/components/com_input/input.js
@@ -3,105 +3,126 @@ import inputData from "./inputData.js";
 import React, { useState, Component } from "react";
 import { StyleSheet, Text, TextInput, View, Button } from "react-native";
 
-export class InputPage extends Component{
-    render(){
-        return(
-          <View style = {styles.container}>
-            <View style = {styles.textfields}>
+export class InputScreen extends React.Component {
+  state = {
+    item: '',
+    amount: ''
+  }
+  handleItem = (text) => {
+    this.setState({ item: text })
+  }
+  handleAmount = (text) => {
+    this.setState({ amount: text })
+  }
+  message = (item, pass) => {
+    alert('item: ' + item + ',' + ' amount: ' + pass)
+  }
+  render() {
+    return (
+      <View style={stylesOne.container}> 
+        {/* <Text style = {stylesOne.headerInputText}>Input Inventory</Text> */}
+        <TextInput style = {stylesOne.input}
+              //  underlineColorAndroid = "transparent"
+            
+               placeholder = "  Item"
+               placeholderTextColor = "#2163f6"
+               autoCapitalize = "none"
+               onChangeText = {this.handleItem}/>
+            
+            <TextInput style = {stylesOne.input}
+              //  underlineColorAndroid = "transparent"
+               placeholder = "  Amount"
+               placeholderTextColor = "#2163f6"
+               autoCapitalize = "none"
+               onChangeText = {this.handleAmount}/>
+            
+            <TouchableOpacity
+               style = {stylesOne.submitButton}
+               onPress = {
+                  () => this.message(this.state.item, this.state.amount)
+               }>
+               <Text style = {stylesOne.submitButtonText}> Submit </Text>
+            </TouchableOpacity>
+      </View>
+    )
+  }
+};
 
-              <TextInput style = {styles.input}
-                placeholder = "Item"
-                keyboardType = "default"
-                autoCapitalize = "none"
-                autoCorrect = {false}
-              />
 
-              <TextInput style = {styles.input}
-                placeholder = "Units (lbs, ml, piece)"
-                keyboardType = "email-default"
-                autoCapitalize = "none"
-                autoCorrect = {false}
-              />
+let testArr = {
 
-              <TextInput style = {styles.input}
-                placeholder = "Units (lbs, ml, piece)"
-                keyboardType = "email-default"
-                autoCapitalize = "none"
-                autoCorrect = {false}
-              />
+  "tomato": {
+    unit: "whole tomatoes",
 
-              <Button 
-                title = "Add Item"
-                color = "#1abc9c"
-                onPress = {() => this.props.navigation.navigate('Register')}
-              />
-
-            </View>
-          </View>
-        )
+    expiration: {
+      "date1": "30",
+      "Date2": "21"
     }
-}
+
+  }
+};
+
+saveItem(eggs, 20);
+
+function saveItem(item_type, item_unit, amount){
+
+  var newItem = {
+    unit: item_unit, //units: eggs
+    amnt: amount //amount: 20
+  };
+
+  testArr[item_type] = newItem;
+
+};
 
 export default InputPage;
 
-export default function App() {
-  const [outputText, setOutputText] = useState("Old Message");
-  const [value, onChangeText] = React.useState('Useless Placeholder');
+// export default function App() {
+//   const [outputText, setOutputText] = useState("Old Message");
+//   const [value, onChangeText] = React.useState('Useless Placeholder');
 
-  return (
-    <View style={styles.container}>
-      <Text>Item: </Text>
-      <TextInput
-      style={{ height: 40, borderColor: 'gray', borderWidth: 2 }}
-      onChangeText={text => onChangeText(text)}
-      value={value}
-    />
+//   return (
+//     <View style={styles.container}>
+//       <Text>Item: </Text>
+//       <TextInput
+//       style={{ height: 40, borderColor: 'gray', borderWidth: 2 }}
+//       onChangeText={text => onChangeText(text)}
+//       value={value}
+//     />
 
-    <Button
-        title="Add"
-        onPress={() => setOutputText("the text changed!")}
-      />
+//     <Button
+//         title="Add"
+//         onPress={() => setOutputText("the text changed!")}
+//       />
 
-      <Text>{outputText}</Text>
+//       <Text>{outputText}</Text>
       
-    </View>
-  );
-}
+//     </View>
+//   );
+// }
 
-const styles = StyleSheet.create({
-
+// this is the styling for the input page
+const stylesOne = StyleSheet.create({
   container: {
-    flex: 1,
-    padding: 20,
-    backgroundColor: "blue",
-    alignItems: "stretch",
-    justifyContent: "center"
+    marginTop: 250,
+    display: 'flex',
+    alignContent: 'center',
+    justifyContent: 'center',
   },
-
   input: {
-    paddingLeft: 20,
-    borderRadius: 50,
-    fontSize: 20,
-    height: 50,
-    fontSize: 25,
-    backgroundColor: 'white',
-    borderWidth: 1,
-    marginBottom: 20,
-    color: "#34495e"
+     margin: 15,
+     height: 40,
+     borderColor: '#2163f6',
+     borderWidth: 1
   },
-
-  buttoncontainer: {
-    height: 50,
-    borderRadius: 50,
-    backgroundColor: '#1abc9c',
-    paddingVertical: 10,
-    justifyContent: 'center'
+  submitButton: {
+     backgroundColor: '#2163f6',
+     padding: 10,
+     margin: 15,
+     height: 40,
   },
-
-  buttontext: {
-    textAlign: 'center',
-    color: '#ecf0f1',
-    fontSize: 20
-  }
+  submitButtonText:{
+     color: 'white'
+  },
 
 });
