483ed294d56774f53505b077660ab1c66a2e0bb0
blazeris <blazeris@users.noreply.github.com>
2020-03-28 12:04:30 -0700

    Added parts of sectionlists

--- rn-first-app/components/com_invent/invent.js
+++ rn-first-app/components/com_invent/invent.js
@@ -1,74 +1,114 @@
-//import invent_style from "./invent_style.js";
-const fs = require("fs");
+import React, { Component } from "react";
+import {
+  AppRegistry,
+  SectionList,
+  StyleSheet,
+  Text,
+  View,
+  Alert,
+  Platform
+} from "react-native";
+import { readJSON } from "./inventDataReader";
 
+var listData = {
+  data: {},
+  title: "Ration Inventory"
+};
 /*
-class InventPage extends Component {
-    render() {
-        return (
-            <Text></Text>
-        )
+listData = {
+  data: {
+    {
+      itemName: "Tomato",
+      unit: "oz",
+      expiration: {
+
+      }
     }
+  },
+  title: "Ration Inventory"
 }
 */
 
-//reads the JSON file and returns it in object form
-function readJSON() {
-  console.log("Reading data.json");
-  let raw = fs.readFileSync(__dirname + "/../../data.json");
-  return JSON.parse(raw);
-}
-
-//Searches the data.JSON file and object data of the target if it exists
-function searchJSON(target) {
-  let json = readJSON();
-  console.log("Searching data for " + target);
-  target = target.toLowerCase().trim();
-
-  //loops through all keys to determine a match
-  for (i = 0; i < Object.keys(json).length; i++) {
-    if (Object.keys(json)[i] == target) {
-      console.log("Found " + Object.keys(json)[i]);
-      return Object.values(json)[i];
-    }
+class SectionListItem extends Component {
+  render() {
+    return (
+      <View
+        style={{
+          flex: 1,
+          flexDirection: "column",
+          backgroundColor: "blue"
+        }}
+      >
+        <Text
+          style={{
+            fontSize: 16,
+            fontWeight: "bold",
+            color: "black",
+            marginLeft: 20,
+            marginRight: 10
+          }}
+        >
+          {" "}
+          {this.props.item.name}
+        </Text>
+        <Text
+          style={{
+            fontSize: 16,
+            marginLeft: 20,
+            marginRight: 10
+          }}
+        >
+          {" "}
+          {this.props.item.description}
+        </Text>
+      </View>
+    );
   }
-  //when no matching key is found, defaults to undefined
-  console.log("Item " + target + " not found");
-  return undefined;
 }
 
-function findTotalAmount(target) {
-  console.log("\nBeginning to find total quantity amount of " + target);
-  let item = searchJSON(target);
-  //error checking, if the item actually exists in JSON
-  if (item != undefined) {
-    let sum = 0;
-    console.log("Summing up amounts");
-    //check if there's actual values stored
-    if (Object.values(item["expiration"]) != undefined) {
-      //loop through all values for typical sum finding
-      for (i = 0; i < Object.values(item["expiration"]).length; i++) {
-        let expireDate = Object.keys(item["expiration"])[i];
-        let quantity = Object.values(item["expiration"])[i];
-
-        //removes key:value pair if it is past the expiration date
-        if (Date.parse(expireDate) <= Date.now()) {
-          console.log("Removed " + expireDate + ", " + quantity + " " + target);
-          delete expireDate;
-        }
-        //otherwise proceed as normal and sum it up
-        else {
-          sum += parseInt(quantity);
-        }
-      }
-      console.log("Total amount of target " + target + " is " + sum);
-      return sum;
-    }
+class SectionHeader extends Component {
+  render() {
+    return (
+      <View
+        style={{
+          flex: 1,
+          backgroundColor: "dark blue"
+        }}
+      >
+        <Text
+          style={{
+            fontSize: 16,
+            fontWeight: "bold",
+            color: "white",
+            marginLeft: 20,
+            marginRight: 10
+          }}
+        >
+          {this.props.section.title}
+        </Text>
+      </View>
+    );
   }
-  //default to 0 when it doesn't exist
-  console.log("No amounts of " + target + " exist");
-  return 0;
 }
 
-console.log(findTotalAmount("tomato"));
-
-//export default InventPage;
+export default class InventSectionList extends Component {
+  render() {
+    return (
+      <View style={{ flex: 1 }}>
+        <SectionList
+          renderItem={({ item, index }) => {
+            return (
+              <SectionListItem item={item} index={index}></SectionListItem>
+            );
+          }}
+          renderSectionHeader={({ section }) => {
+            return <SectionHeader section={section} />;
+          }}
+          keyExtractor={(item, index) => item.name}
+        >
+          onRefresh = {{}}
+        </SectionList>
+      </View>
+    );
+  }
+}
---
+++ rn-first-app/components/com_invent/inventDataReader.js
@@ -0,0 +1,74 @@
+//import invent_style from "./invent_style.js";
+const fs = require("fs");
+
+/*
+class InventPage extends Component {
+    render() {
+        return (
+            <Text></Text>
+        )
+    }
+}
+*/
+
+//reads the JSON file and returns it in object form
+function readJSON() {
+  console.log("Reading data.json");
+  let raw = fs.readFileSync(__dirname + "/../../data.json");
+  return JSON.parse(raw);
+}
+
+//Searches the data.JSON file and object data of the target if it exists
+function searchJSON(target) {
+  let json = readJSON();
+  console.log("Searching data for " + target);
+  target = target.toLowerCase().trim();
+
+  //loops through all keys to determine a match
+  for (i = 0; i < Object.keys(json).length; i++) {
+    if (Object.keys(json)[i] == target) {
+      console.log("Found " + Object.keys(json)[i]);
+      return Object.values(json)[i];
+    }
+  }
+  //when no matching key is found, defaults to undefined
+  console.log("Item " + target + " not found");
+  return undefined;
+}
+
+function findTotalAmount(target) {
+  console.log("\nBeginning to find total quantity amount of " + target);
+  let item = searchJSON(target);
+  //error checking, if the item actually exists in JSON
+  if (item != undefined) {
+    let sum = 0;
+    console.log("Summing up amounts");
+    //check if there's actual values stored
+    if (Object.values(item["expiration"]) != undefined) {
+      //loop through all values for typical sum finding
+      for (i = 0; i < Object.values(item["expiration"]).length; i++) {
+        let expireDate = Object.keys(item["expiration"])[i];
+        let quantity = Object.values(item["expiration"])[i];
+
+        //removes key:value pair if it is past the expiration date
+        if (Date.parse(expireDate) <= Date.now()) {
+          console.log("Removed " + expireDate + ", " + quantity + " " + target);
+          delete expireDate;
+        }
+        //otherwise proceed as normal and sum it up
+        else {
+          sum += parseInt(quantity);
+        }
+      }
+      console.log("Total amount of target " + target + " is " + sum);
+      return sum;
+    }
+  }
+  //default to 0 when it doesn't exist
+  console.log("No amounts of " + target + " exist");
+  return 0;
+}
+
+console.log(findTotalAmount("tomato"));
+
+export default InventPage;
