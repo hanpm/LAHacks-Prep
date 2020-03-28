b1e5f691979c1c2580db76d8246cebfab5d9372a
githelsui <githelsuico@gmail.com>
2020-03-28 03:31:28 -0700

    i hate it here

--- rn-first-app/components/com_input/input.js
+++ rn-first-app/components/com_input/input.js
@@ -2,6 +2,27 @@ import input_style from "./input_style.js";
 import React, { useState, Component } from "react";
 import { StyleSheet, Text, TextInput, View, Button } from "react-native";
 
+export class InputPage extends Component{
+    render(){
+        return(
+          <View style = {styles.container}>
+            <View style = {styles.textfields}>
+              <TextInput style = {styles.input}>
+                placeholder = "username"
+                returnKeyType = "next"
+                onSubmitEditing = {() => this.passwordInput.focus()}
+                keyboardType = "email-address"
+                autoCapitalize = "none"
+                autoCorrect = {false}
+              </TextInput>
+            </View>
+          </View>
+        )
+    }
+}
+
+export default InputPage;
+
 export default function App() {
   const [outputText, setOutputText] = useState("Old Message");
   const [value, onChangeText] = React.useState('Useless Placeholder');
@@ -27,10 +48,39 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
+
   container: {
     flex: 1,
+    padding: 20,
     backgroundColor: "blue",
-    alignItems: "center",
+    alignItems: "stretch",
     justifyContent: "center"
+  },
+
+  input: {
+    paddingLeft: 20,
+    borderRadius: 50,
+    fontSize: 20,
+    height: 50,
+    fontSize: 25,
+    backgroundColor: 'white',
+    borderWidth: 1,
+    marginBottom: 20,
+    color: "#34495e"
+  },
+
+  buttoncontainer: {
+    height: 50,
+    borderRadius: 50,
+    backgroundColor: '#1abc9c',
+    paddingVertical: 10,
+    justifyContent: 'center'
+  },
+
+  buttontext: {
+    textAlign: 'center',
+    color: '#ecf0f1',
+    fontSize: 20
   }
+
 });
