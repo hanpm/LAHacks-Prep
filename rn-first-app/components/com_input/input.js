70f0121074c4a3f243ce45b7f9db927d6361f5c5
githelsui <githelsuico@gmail.com>
2020-03-28 00:19:22 -0700

    node npm

--- rn-first-app/components/com_input/input.js
+++ rn-first-app/components/com_input/input.js
@@ -1,1 +1,25 @@
 import input_style from "./input_style.js";
+import React, { useState } from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
+
+export default function App() {
+  const [outputText, setOutputText] = useState("Open App.js");
+  return (
+    <View style={styles.container}>
+      <Text>{outputText}</Text>
+      <Button
+        title="Change Text"
+        onPress={() => setOutputText("the text changed!")}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "blue",
+    alignItems: "center",
+    justifyContent: "center"
+  }
+});
