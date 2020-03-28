1e49a56e6a8af06fd884406ce8833ea8ca9913fc
davidnguyenc <davidcn2@uci.edu>
2020-03-28 02:28:14 -0700

    added navigation bar

--- rn-first-app/App.js
+++ rn-first-app/App.js
@@ -1,97 +1,130 @@
-import React, { useState } from "react";
-import { StyleSheet, Text, View, Button } from "react-native";
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
 
-export default function App() {
-  const [outputText, setOutputText] = useState("Open App.js");
-  return (
-    <View style={styles.container}>
-      <Text>{outputText}</Text>
-      <Button
-        title="Change Text"
-        onPress={() => setOutputText("the text changed!")}
-      />
-    </View>
-  );
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+import Icon from 'react-native-vector-icons/Ionicons';
+import {createAppContainer} from 'react-navigation';
+import {createMaterialBottomTabNavigator} from 'react-navigation-material-bottom-tabs';
+
+class HomeScreen extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>HomeScreen</Text>
+      </View>
+    )
+  }
+}
+
+class ProfileScreen extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>ProfileScreen</Text>
+      </View>
+    )
+  }
+}
+
+class HistoryScreen extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>HistoryScreen</Text>
+      </View>
+    )
+  }
+}
+
+class CartScreen extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>CartScreen</Text>
+      </View>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "blue",
-    alignItems: "center",
-    justifyContent: "center"
+    justifyContent: 'center',
+    alignItems: 'center'
   }
 });
 
-//LOL this was david's code roflmao
+const TabNavigator = createMaterialBottomTabNavigator(
+  {
+    Home: {
+      screen: HomeScreen,
+      navigationOptions: {
+        tabBarIcon: ({ tintColor }) => (
+          <View>
+            <Icon style={[{color: tintColor}]} size={25} name={'ios-home'} />
+          </View>
+        ),
+      }
+    },
+    Profile: {
+      screen: ProfileScreen,
+      navigationOptions: {
+        tabBarIcon: ({ tintColor }) => (
+          <View>
+            <Icon style={[{color: tintColor}]} size={25} name={'ios-person'} />
+          </View>
+        ),
+        activeColor: '#ffffff',
+        inactiveColor: '#a3c2fa',
+        barStyle: { backgroundColor: '#2163f6' },
+      }
+    },
+    History: {
+      screen: HistoryScreen,
+      navigationOptions: {
+        tabBarIcon: ({ tintColor }) => (
+          <View>
+            <Icon style={[{color: tintColor}]} size={25} name={'ios-images'} />
+          </View>
+        ),
+        activeColor: '#ffffff',
+        inactiveColor: '#92c5c2',
+        barStyle: { backgroundColor: '#2c6d6a' },
+      }
+    },
+    Cart: {
+      screen: CartScreen,
+      navigationOptions: {
+        tabBarIcon: ({ tintColor }) => (
+          <View>
+            <Icon style={[{color: tintColor}]} size={25} name={'ios-cart'} />
+          </View>
+        ),
+        activeColor: '#ffffff',
+        inactiveColor: '#ebaabd',
+        barStyle: { backgroundColor: '#d13560' },
+      }
+    },
+  },
+  {
+    initialRouteName: 'Home',
+    activeColor: '#ffffff',
+    inactiveColor: '#bda1f7',
+    barStyle: { backgroundColor: '#6948f4' },
+  }
+);
 
-// import * as React from 'react';
-// import { Platform, StyleSheet, Text, View } from 'react-native';
-
-// const instructions = Platform.select({
-//   ios: `Press Cmd+R to reload,\nCmd+D or shake for dev menu`,
-//   android: `Double tap R on your keyboard to reload,\nShake or press menu button for dev menu`,
-// });
-
-// export default function App() {
-//   return (
-//     <View style={styles.container}>
-//       <Text style={styles.welcome}>Welcome to React Native!</Text>
-//       <Text style={styles.instructions}>To get started, edit App.js</Text>
-//       <Text style={styles.instructions}>{instructions}</Text>
-//     </View>
-//   );
-// }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     justifyContent: 'center',
-//     alignItems: 'center',
-//     backgroundColor: '#F5FCFF',
-//   },
-//   welcome: {
-//     fontSize: 20,
-//     textAlign: 'center',
-//     margin: 10,
-//   },
-//   instructions: {
-//     textAlign: 'center',
-//     color: '#333333',
-//     marginBottom: 5,
-//   },
-// });
-
-// import * as React from 'react';
-// import { Text, View } from 'react-native';
-// import { NavigationContainer } from '@react-navigation/native';
-// import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-
-// function HomeScreen() {
-//   return (
-//     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-//       <Text>Home!</Text>
-//     </View>
-//   );
-// }
-
-// function SettingsScreen() {
-//   return (
-//     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-//       <Text>Settings!</Text>
-//     </View>
-//   );
-// }
-
-// const Tab = createBottomTabNavigator();
-
-// export default function App() {
-//   return (
-//     <NavigationContainer>
-//       <Tab.Navigator>
-//         <Tab.Screen name="Home" component={HomeScreen} />
-//         <Tab.Screen name="Settings" component={SettingsScreen} />
-//       </Tab.Navigator>
-//     </NavigationContainer>
-//   );
-// }
+export default createAppContainer(TabNavigator);
