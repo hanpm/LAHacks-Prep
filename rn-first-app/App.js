ba32ab697fd07ed80b9682e55eb55205915c5c60
Han Pham <hanpham@Hans-MacBook-Pro.local>
2020-03-27 18:40:14 -0700

    demo

---
+++ rn-first-app/App.js
@@ -0,0 +1,92 @@
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+export default function App() {
+  const [outputText, setOutputText] = useState('Open App.js');
+  return (
+    <View style={styles.container}>
+      <Text>{outputText}</Text>
+      <Button title="Change Text" onPress={() => setOutputText('the text changed!')} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+// import * as React from 'react';
+// import { Platform, StyleSheet, Text, View } from 'react-native';
+
+// const instructions = Platform.select({
+//   ios: `Press Cmd+R to reload,\nCmd+D or shake for dev menu`,
+//   android: `Double tap R on your keyboard to reload,\nShake or press menu button for dev menu`,
+// });
+
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Text style={styles.welcome}>Welcome to React Native!</Text>
+//       <Text style={styles.instructions}>To get started, edit App.js</Text>
+//       <Text style={styles.instructions}>{instructions}</Text>
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//     backgroundColor: '#F5FCFF',
+//   },
+//   welcome: {
+//     fontSize: 20,
+//     textAlign: 'center',
+//     margin: 10,
+//   },
+//   instructions: {
+//     textAlign: 'center',
+//     color: '#333333',
+//     marginBottom: 5,
+//   },
+// });
+
+// import * as React from 'react';
+// import { Text, View } from 'react-native';
+// import { NavigationContainer } from '@react-navigation/native';
+// import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+// function HomeScreen() {
+//   return (
+//     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+//       <Text>Home!</Text>
+//     </View>
+//   );
+// }
+
+// function SettingsScreen() {
+//   return (
+//     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+//       <Text>Settings!</Text>
+//     </View>
+//   );
+// }
+
+// const Tab = createBottomTabNavigator();
+
+// export default function App() {
+//   return (
+//     <NavigationContainer>
+//       <Tab.Navigator>
+//         <Tab.Screen name="Home" component={HomeScreen} />
+//         <Tab.Screen name="Settings" component={SettingsScreen} />
+//       </Tab.Navigator>
+//     </NavigationContainer>
+//   );
+// }
