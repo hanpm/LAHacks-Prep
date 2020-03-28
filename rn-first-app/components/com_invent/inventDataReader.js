13cf7fd0af16c11bcedffc01debca68a58a3ff30
blazeris <blazeris@users.noreply.github.com>
2020-03-28 16:01:35 -0700

    asynchronous hell

--- rn-first-app/components/com_invent/inventDataReader.js
+++ rn-first-app/components/com_invent/inventDataReader.js
@@ -16,63 +16,78 @@ function readJSON() {
 }
 */
 
-async () => {
-  try {
-    await AsyncStorage.setItem("inventory", {
-      "test-type": {
-        unit: null,
-        expiration: {
-          date: "amount"
-        }
-      },
-      tomato: {
-        unit: "whole tomatoes",
-        expiration: {
-          "3/29/2020": "30",
-          "4/20/2020": "15",
-          "4/13/2020": "4",
-          null: "40"
-        }
-      }
-    });
-  } catch (error) {}
-};
-
 //Searches the storage object and object data of the target if it exists
 function searchStorage(target) {
-  let storage = async () => {
-    try {
-      const value = await AsyncStorage.getItem("inventory");
-      if (value !== null) {
-        console.log(value);
+  let test = {
+    tomato: {
+      unit: "whole tomatoes",
+      expiration: {
+        "3/29/2020": "30",
+        "4/20/2020": "15",
+        "4/13/2020": "4"
       }
-    } catch (error) {}
+    }
   };
 
+  store = async () => {
+    try {
+      await AsyncStorage.setItem("inventory", JSON.stringify(test));
+    } catch (error) {}
+  };
+  store();
   console.log("Searching data for " + target);
   target = target.toLowerCase().trim();
 
-  //loops through all keys to determine a match
-  for (i = 0; i < Object.keys(storage).length; i++) {
-    if (Object.keys(storage)[i] == target) {
-      console.log("Found " + Object.keys(storage)[i]);
-      return Object.values(storage)[i];
+  access = async () => {
+    try {
+      console.log("Asynchronously accessing storage");
+      let value = await AsyncStorage.getItem("inventory");
+      let storage = JSON.parse(value);
+
+      console.log(storage);
+      console.log(Object.keys(storage));
+      console.log(Object.values(storage));
+      /*
+      //loops through all keys to determine a match
+      for (i = 0; i < Object.keys(storage).length; i++) {
+        
+        if (Object.keys(storage)[i] == target) {
+          console.log("Found " + Object.keys(storage)[i]);
+          return Object.values(storage)[i];
+        }
+      }
+
+      */
+
+      let item = storage[target];
+      if (item != undefined) {
+        console.log("Found");
+        console.log(item);
+        return item;
+      }
+    } catch (error) {
+      console.log(error);
     }
-  }
-  //when no matching key is found, defaults to undefined
-  console.log("Item " + target + " not found");
-  return undefined;
+    //when no matching key is found, defaults to undefined
+    console.log("Item " + target + " not found");
+    return undefined;
+  };
+
+  return access();
 }
 
 function findTotalAmount(target) {
   console.log("\nBeginning to find total quantity amount of " + target);
   let item = searchStorage(target);
   //error checking, if the item actually exists in storage
-  if (item != undefined) {
+  if (item != undefined && item != null) {
     let sum = 0;
     console.log("Summing up amounts");
     //check if there's actual values stored
-    if (Object.values(item["expiration"]) != undefined) {
+    if (
+      item["expiration"] != undefined &&
+      Object.values(item["expiration"]) != undefined
+    ) {
       //loop through all values for typical sum finding
       for (i = 0; i < Object.values(item["expiration"]).length; i++) {
         let expireDate = Object.keys(item["expiration"])[i];
@@ -81,7 +96,7 @@ function findTotalAmount(target) {
         //removes key:value pair if it is past the expiration date
         if (Date.parse(expireDate) <= Date.now()) {
           console.log("Removed " + expireDate + ", " + quantity + " " + target);
-          delete Object.keys(item["expiration"])[i];
+          //delete Object.keys(item["expiration"])[i];
         }
         //otherwise proceed as normal and sum it up
         else {
