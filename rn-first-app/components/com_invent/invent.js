874ad88887db60bc417d921695eb1a23ea1f7b70
githelsui <githelsuico@gmail.com>
2020-03-28 20:38:57 -0700

    ha

--- rn-first-app/components/com_invent/invent.js
+++ rn-first-app/components/com_invent/invent.js
@@ -1,109 +1,109 @@
-import React, { Component } from "react";
-import {
-  AppRegistry,
-  SectionList,
-  StyleSheet,
-  Text,
-  View,
-  Alert,
-  Platform
-} from "react-native";
+// import React, { Component } from "react";
+// import {
+//   AppRegistry,
+//   SectionList,
+//   StyleSheet,
+//   Text,
+//   View,
+//   Alert,
+//   Platform
+// } from "react-native";
 
-/*
-listData = {
-  data: {
-    {
-      itemName: "Tomato",
-      unit: "oz",
-      expiration: {
+// /*
+// listData = {
+//   data: {
+//     {
+//       itemName: "Tomato",
+//       unit: "oz",
+//       expiration: {
 
-      }
-    }
-  },
-  title: "Ration Inventory"
-}
-*/
+//       }
+//     }
+//   },
+//   title: "Ration Inventory"
+// }
+// */
 
-class SectionListItem extends Component {
-  render() {
-    return (
-      <View
-        style={{
-          flex: 1,
-          flexDirection: "column",
-          backgroundColor: "blue"
-        }}
-      >
-        <Text
-          style={{
-            fontSize: 16,
-            fontWeight: "bold",
-            color: "black",
-            marginLeft: 20,
-            marginRight: 10
-          }}
-        >
-          {" "}
-          {this.props.item.name}
-        </Text>
-        <Text
-          style={{
-            fontSize: 16,
-            marginLeft: 20,
-            marginRight: 10
-          }}
-        >
-          {" "}
-          {this.props.item.description}
-        </Text>
-      </View>
-    );
-  }
-}
+// class SectionListItem extends Component {
+//   render() {
+//     return (
+//       <View
+//         style={{
+//           flex: 1,
+//           flexDirection: "column",
+//           backgroundColor: "blue"
+//         }}
+//       >
+//         <Text
+//           style={{
+//             fontSize: 16,
+//             fontWeight: "bold",
+//             color: "black",
+//             marginLeft: 20,
+//             marginRight: 10
+//           }}
+//         >
+//           {" "}
+//           {this.props.item.name}
+//         </Text>
+//         <Text
+//           style={{
+//             fontSize: 16,
+//             marginLeft: 20,
+//             marginRight: 10
+//           }}
+//         >
+//           {" "}
+//           {this.props.item.description}
+//         </Text>
+//       </View>
+//     );
+//   }
+// }
 
-class SectionHeader extends Component {
-  render() {
-    return (
-      <View
-        style={{
-          flex: 1,
-          backgroundColor: "dark blue"
-        }}
-      >
-        <Text
-          style={{
-            fontSize: 16,
-            fontWeight: "bold",
-            color: "white",
-            marginLeft: 20,
-            marginRight: 10
-          }}
-        >
-          {this.props.section.title}
-        </Text>
-      </View>
-    );
-  }
-}
+// class SectionHeader extends Component {
+//   render() {
+//     return (
+//       <View
+//         style={{
+//           flex: 1,
+//           backgroundColor: "dark blue"
+//         }}
+//       >
+//         <Text
+//           style={{
+//             fontSize: 16,
+//             fontWeight: "bold",
+//             color: "white",
+//             marginLeft: 20,
+//             marginRight: 10
+//           }}
+//         >
+//           {this.props.section.title}
+//         </Text>
+//       </View>
+//     );
+//   }
+// }
 
-export default class InventSectionList extends Component {
-  render() {
-    return (
-      <View style={{ flex: 1 }}>
-        <SectionList
-          renderItem={({ item, index }) => {
-            return (
-              <SectionListItem item={item} index={index}></SectionListItem>
-            );
-          }}
-          renderSectionHeader={({ section }) => {
-            return <SectionHeader section={section} />;
-          }}
-          keyExtractor={(item, index) => item.name}
-        >
-          onRefresh = {{}}
-        </SectionList>
-      </View>
-    );
-  }
-}
+// export default class InventSectionList extends Component {
+//   render() {
+//     return (
+//       <View style={{ flex: 1 }}>
+//         <SectionList
+//           renderItem={({ item, index }) => {
+//             return (
+//               <SectionListItem item={item} index={index}></SectionListItem>
+//             );
+//           }}
+//           renderSectionHeader={({ section }) => {
+//             return <SectionHeader section={section} />;
+//           }}
+//           keyExtractor={(item, index) => item.name}
+//         >
+//           onRefresh = {{}}
+//         </SectionList>
+//       </View>
+//     );
+//   }
+// }
