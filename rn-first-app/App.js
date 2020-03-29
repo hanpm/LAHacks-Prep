ef501d583b52a438d2ec3a6907f774580c1e186d
Han Pham <hanpham@Hans-MacBook-Pro.local>
2020-03-28 23:27:23 -0700

    made the input page, added styling

--- rn-first-app/App.js
+++ rn-first-app/App.js
@@ -7,7 +7,8 @@ import {
   Text,
   StatusBar,
   TextInput,
-  TouchableOpacity
+  TouchableOpacity,
+
 } from 'react-native';
 
 import {
@@ -17,6 +18,8 @@ import {
   DebugInstructions,
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
+
+
 import Icon from 'react-native-vector-icons/Ionicons';
 import {createAppContainer} from 'react-navigation';
 import {createMaterialBottomTabNavigator} from 'react-navigation-material-bottom-tabs';
@@ -35,7 +38,10 @@ class HomeScreen extends React.Component {
 class InputScreen extends React.Component {
   state = {
     item: '',
-    amount: ''
+    amount: '',
+    month: '',
+    day: '', 
+    year:''
   }
   handleItem = (text) => {
     this.setState({ item: text })
@@ -43,34 +49,72 @@ class InputScreen extends React.Component {
   handleAmount = (text) => {
     this.setState({ amount: text })
   }
-  message = (item, pass) => {
-    alert('item: ' + item + ',' + ' amount: ' + pass)
+
+  handleMonth = (text) => {
+    this.setState({ month: text})
   }
+  handleDay = (text) => {
+    this.setState({ day: text})
+  }
+  handleYear = (text) => {
+    this.setState({ year: text})
+  }
+
+  message = (item, amount, month, day, year) => {
+    alert('item: ' + item + ',' + ' amount: ' + amount +  "," + " date: " + month + "/" + day + '/' + year)
+  }
+  
   render() {
     return (
+      
       <View style={stylesOne.container}> 
+      <Text style= {stylesOne.headerTitleText}>Food Input</Text>
         {/* <Text style = {stylesOne.headerInputText}>Input Inventory</Text> */}
         <TextInput style = {stylesOne.input}
-              //  underlineColorAndroid = "transparent"
-            
                placeholder = "  Item"
                placeholderTextColor = "#2163f6"
                autoCapitalize = "none"
                onChangeText = {this.handleItem}/>
-            
             <TextInput style = {stylesOne.input}
-              //  underlineColorAndroid = "transparent"
                placeholder = "  Amount"
                placeholderTextColor = "#2163f6"
                autoCapitalize = "none"
                onChangeText = {this.handleAmount}/>
+              
+           <Text style= {stylesOne.headerExpirationText}>Expiration Date</Text>
+           
+          <View style ={stylesOne.containerDate}>
+              <View style= {stylesOne.containerItem}>
+              <TextInput style = {stylesOne.inputOne}
+                     placeholder = "  Month"
+                     placeholderTextColor = "#2163f6"
+                     autoCapitalize = "none"
+                     onChangeText = {this.handleMonth}/>
+                  </View>   
+              <View style = {stylesOne.containerItem}>
+                    <TextInput style = {stylesOne.inputOne}
+                      placeholder = "  Day"
+                      placeholderTextColor = "#2163f6"
+                      autoCapitalize = "none"
+                      onChangeText = {this.handleDay}/>
+                    </View>
+              <View style = {stylesOne.containerItem}>
+                  <TextInput style = {stylesOne.inputOne}
+                     placeholder = "  Year"
+                     placeholderTextColor = "#2163f6"
+                     autoCapitalize = "none"
+                     onChangeText = {this.handleYear}/>
+              </View>
+            </View>
             
             <TouchableOpacity
                style = {stylesOne.submitButton}
                onPress = {
-                  () => this.message(this.state.item, this.state.amount)
+                  () => this.message(this.state.item, this.state.amount, this.state.month, this.state.day, this.state.year)
                }>
-               <Text style = {stylesOne.submitButtonText}> Submit </Text>
+               
+               <Text style = {stylesOne.submitButtonText}> ⇨ Submit </Text>
+              
             </TouchableOpacity>
       </View>
     )
@@ -105,6 +149,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center'
+   
   }
 });
 
@@ -112,28 +157,95 @@ const styles = StyleSheet.create({
 // this is the styling for the input page
 const stylesOne = StyleSheet.create({
   container: {
-    marginTop: 250,
+    marginTop: 100,
     display: 'flex',
     alignContent: 'center',
     justifyContent: 'center',
+
+  },
+  containerDate: {
+    display: 'flex',
+    // flexWrap: 'wrap',
+    justifyContent: 'space-between',
+    // alignContent: 'space-between',
+    flexDirection: 'row',
     // borderColor: 'blue',
     // borderWidth: 1,
 
   },
+
+
+  containerItem: {
+    display: 'flex',
+    alignItems: 'flex-start',
+    justifyContent: 'space-between',
+    flexDirection: "row",
+    // alignItems: 'center',
+    borderColor: 'blue',
+    borderWidth: 1,
+    paddingLeft: 20,
+    paddingRight: 20,
+    margin: 25,
+    // borderRadius: 10,
+  },
+
+  headerTitleText: {
+    color: '#2163f6',
+    fontSize: 25,
+    fontWeight: 'bold',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: "center",
+    textAlign: 'center'
+  
+  },
+
+  headerExpirationText: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: "center",
+    textAlign: 'center',
+    marginTop: 40,
+    marginBottom: -2,
+    margin: 15,
+    color:'#2163f6',
+    fontSize: 25,
+    fontWeight: 'bold',
+    
+  },
+
+
   input: {
      margin: 15,
-     height: 40,
+     height: 30,
      borderColor: '#2163f6',
      borderWidth: 1
   },
+
+  inputOne: {
+    alignItems: "center",
+    height: 40,
+    width: 50,
+    borderColor: '#2163f6',
+ },
   submitButton: {
+    display: 'flex',
+    justifyContent: 'center',
      backgroundColor: '#2163f6',
      padding: 10,
-     margin: 15,
+     marginLeft: 35,
+     marginRight: 35,
+     
      height: 40,
+    //  width: 300, 
+     borderRadius: 20,
+
+  
   },
   submitButtonText:{
-     color: 'white'
+     color: 'white',
+     textAlign: 'center',
+
   },
 
   //trying to add header --> ignore for now
