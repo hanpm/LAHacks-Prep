74a1b3a3d7043bead1480e12c101366716e1afff
githelsui <githelsuico@gmail.com>
2020-03-28 22:31:38 -0700

    inputdata

--- rn-first-app/components/com_input/inputData.js
+++ rn-first-app/components/com_input/inputData.js
@@ -1,66 +1,66 @@
-// import input from "./input.js";
-// import {newItem} from "./input.js";
-// import { Text, AsyncStorage } from "react-native";
-// import React, { Component } from "react";
+import input from "./input.js";
+import {newItem} from "./input.js";
+import { Text, AsyncStorage } from "react-native";
+import React, { Component } from "react";
 
-// var items_list = {
+var items_list = {
 
-//   itemname: {
-//     unit: "unit type",
-//     content: [
+  itemname: {
+    unit: "unit type",
+    content: [
       
-//       {
-//         expiration: "date",
-//         amount: "number"
-//       }
+      {
+        expiration: "date",
+        amount: "number"
+      }
 
-//     ]
-//   }
-// };
+    ]
+  }
+};
 
-// addItem = (item_type, item_unit, amount, expiration_date) => {
+addItem = (item_type, item_unit, amount, expiration_date) => {
 
-//   var newItem = {
-//     unit: item_unit, //units: eggs
-//     content: [
-//       {
-//         expiration: expiration_date,
-//         amnt: amount //amount: 20
-//       }
-//     ]
-//   };
+  var newItem = {
+    unit: item_unit, //units: eggs
+    content: [
+      {
+        expiration: expiration_date,
+        amnt: amount //amount: 20
+      }
+    ]
+  };
 
-//   items_list[item_type] = newItem;
-//   saveData("items list", items_list);
-//   logData();
+  items_list[item_type] = newItem;
+  saveData("items list", items_list);
+  logData();
   
-// };
+};
 
-// useAmount = (item_type, amountUsed) => {
+useAmount = (item_type, amountUsed) => {
 
-//   var newAmount = item_type.content[i].amount - amountUsed;
+  var newAmount = item_type.content[i].amount - amountUsed;
 
-//   items_list[item_type].content[i].amount = newAmount;
+  items_list[item_type].content[i].amount = newAmount;
   
-//   saveData("items list", items_list);
+  saveData("items list", items_list);
   
-// };
+};
 
-// logData = () => {
+logData = () => {
 
-//   for (var key in items_list) {
-//     // check if the property/key is defined in the object itself, not in parent
-//     if (dictionary.hasOwnProperty(key)) {           
-//         console.log(key, dictionary[key]);
-//     }
-// }
+  for (var key in items_list) {
+    // check if the property/key is defined in the object itself, not in parent
+    if (dictionary.hasOwnProperty(key)) {           
+        console.log(key, dictionary[key]);
+    }
+}
   
-// };
+};
 
-// saveData = async (item_name, object) => {
-//   try{
-//     await AsyncStorage.setItem(item_name, object);
-//   } catch (error) {
-//       console.log("it didnt work u bitch ");
-//   }
-// };
+saveData = async (item_name, object) => {
+  try{
+    await AsyncStorage.setItem(item_name, object);
+  } catch (error) {
+      console.log("it didnt work u bitch ");
+  }
+};
